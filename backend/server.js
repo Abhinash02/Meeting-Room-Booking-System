382e043327b4361720bf2c535973a82baaa097f0
Abhinash02 <exampaperguide.in@gmail.com>
2026-06-12 19:45:09 +0530

    feat: add server entry point and Vercel configuration for deployment

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import app from './src/app.js';
+import connectDB from './src/config/db.js';
+
+let isConnected = false;
+
+async function ensureDb() {
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
+  }
+}
+
+export default async function handler(req, res) {
+  await ensureDb();
+  return app(req, res);
+}
